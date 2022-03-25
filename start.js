210e2faa42fd8b79ab50105255667df2b3334b51
pekeleku <alumanwalter@gmail.com>
2022-03-25 21:37:33 +0800

    copas dari rio

---
+++ start.js
@@ -0,0 +1,33 @@
+// function segitiga(baris) {
+//   let bintang = "";
+//   for (let i = 0; i <= baris; i++) {
+//     for (let y = baris; y >= i; y--) {
+//       bintang += "*";
+//     }
+//     bintang += "\n";
+//   }
+//   return bintang;
+// }
+// console.log(segitiga(5));
+
+let bintang = "";
+for (let i = 0; i < 5; i++) {
+  for (let y = 5; y > i; y--) {
+    bintang += "*";
+  }
+  bintang += "\n";
+}
+console.log(bintang);
+
+// let bintang = "";
+// for (let i = 1; i <= 3; i++) {
+//   for (let y = 1; y <= 4; y++) {
+//     if ((i == 2 && y == 2) || (i == 2 && y == 3) || (i == 2 && y == 4)) {
+//       bintang += " ";
+//     } else {
+//       bintang += "*";
+//     }
+//   }
+//   bintang += "\n";
+// }
+// console.log(bintang);
